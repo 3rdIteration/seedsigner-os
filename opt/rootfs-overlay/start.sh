185977beb934839325c1500c55c02696433bad6c
3rd Iteration <steve@cryptoguide.tips>
2025-02-25 14:05:00 -0500

    Add bundled GPG pubkeys & import script

--- opt/rootfs-overlay/start.sh
+++ opt/rootfs-overlay/start.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+# Set the date to release so that GPG can work
+/bin/date -s "2025-02-28 12:00"
+
+# Import the bundle of keys 
+/usr/bin/gpg --import pubkeys.asc
+
 cd /opt/src/
 
 #/usr/bin/python3 main.py >> /dev/kmsg 2>&1 &  # version that writes output to dmesg
