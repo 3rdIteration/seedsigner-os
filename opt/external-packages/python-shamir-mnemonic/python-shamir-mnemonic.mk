f632391b80dc47f6a8165f7735b3ff109ee20651
3rd Iteration <steve@cryptoguide.tips>
2025-07-30 21:08:29 -0400

    Update python-shamir-mnemonic to work with setuptools (#15)

--- opt/external-packages/python-shamir-mnemonic/python-shamir-mnemonic.mk
+++ opt/external-packages/python-shamir-mnemonic/python-shamir-mnemonic.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-PYTHON_SHAMIR_MNEMONIC_VERSION = 0.3.0
-PYTHON_SHAMIR_MNEMONIC_SITE = $(call github,trezor,python-shamir-mnemonic,v$(PYTHON_SHAMIR_MNEMONIC_VERSION))
+PYTHON_SHAMIR_MNEMONIC_VERSION = 0.3-setuptools
+PYTHON_SHAMIR_MNEMONIC_SITE = $(call github,3rdIteration,python-shamir-mnemonic,$(PYTHON_SHAMIR_MNEMONIC_VERSION))
 PYTHON_SHAMIR_MNEMONIC_SETUP_TYPE = setuptools
 PYTHON_SHAMIR_MNEMONIC_LICENSE = MIT
 
