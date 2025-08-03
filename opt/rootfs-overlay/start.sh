1669ce9e50d2745ed1403e194ba26ec3d42cd6ba
3rd Iteration <steve@cryptoguide.tips>
2025-08-03 19:15:10 -0400

    Revert "Enable OpenSSL legacy provider for RIPEMD160 (#17)"
    This reverts commit 8a63d2fbbb5f87010986ce83973ccc9982af281e.

--- opt/rootfs-overlay/start.sh
+++ opt/rootfs-overlay/start.sh
@@ -3,13 +3,10 @@
 # Set the date to release so that GPG can work
 /bin/date -s "2025-02-28 12:00"
 
-# Import the bundle of keys
+# Import the bundle of keys 
 /usr/bin/gpg --import /gpg/*.asc
 
 cd /opt/src/
-
-# Ensure OpenSSL loads the legacy provider so RIPEMD160 is available
-export OPENSSL_CONF=/etc/ssl/openssl.cnf
 
 #/usr/bin/python3 main.py >> /dev/kmsg 2>&1 &  # version that writes output to dmesg
 /usr/bin/python3 main.py &
