314b0db35af121f8c9ebd9d5b98ff08147099422
3rd Iteration <steve@cryptoguide.tips>
2025-08-09 16:01:37 -0400

    Launch app before background GPG import (#21)

--- opt/rootfs-overlay/start.sh
+++ opt/rootfs-overlay/start.sh
@@ -3,10 +3,10 @@
 # Set the date to release so that GPG can work
 /bin/date -s "2025-02-28 12:00"
 
-# Import the bundle of keys 
-/usr/bin/gpg --import /gpg/*.asc
-
 cd /opt/src/
 
 #/usr/bin/python3 main.py >> /dev/kmsg 2>&1 &  # version that writes output to dmesg
 /usr/bin/python3 main.py &
+
+# Import the bundle of keys a few seconds after the app starts
+(sleep 5 && /usr/bin/gpg --import /gpg/*.asc) &
