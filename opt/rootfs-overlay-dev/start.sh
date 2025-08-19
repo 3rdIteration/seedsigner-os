fb4d0c98e932c7d36aee94eb07914f0f1d95322d
3rd Iteration <stevekr@gmail.com>
2025-08-19 09:28:53 -0600

    Log dev main output to MicroSD

--- opt/rootfs-overlay-dev/start.sh
+++ opt/rootfs-overlay-dev/start.sh
@@ -5,15 +5,21 @@
 
 if [ -d /mnt/microsd/seedsigner-dev ]; then
     echo "Running SeedSigner from external MicroSD source"
-    cd /mnt/microsd/seedsigner-dev
+    cd /mnt/microsd/seedsigner-dev || exit
     /usr/bin/python3 /usr/bin/microsd_notice.py || true
 else
     echo "Running SeedSigner from embedded source"
-    cd /opt/src/
+    cd /opt/src/ || exit
 fi
 
 #/usr/bin/python3 main.py >> /dev/kmsg 2>&1 &  # version that writes output to dmesg
-/usr/bin/python3 main.py &
+
+LOG_FILE="/mnt/microsd/main.log"
+if [ -d /mnt/microsd ]; then
+    /usr/bin/python3 main.py >> "${LOG_FILE}" 2>&1 &
+else
+    /usr/bin/python3 main.py &
+fi
 
 # Import the bundle of keys a few seconds after the app starts
 (sleep 5 && /usr/bin/gpg --import /gpg/*.asc) &
