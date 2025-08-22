0543370da4848a189045772bdcf9c7e36bbd23ad
3rd Iteration <steve@cryptoguide.tips>
2025-08-22 15:22:40 -0400

    Move GPG key logic into seedsigner app repo

--- opt/rootfs-overlay-dev/start.sh
+++ opt/rootfs-overlay-dev/start.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 
-# Set the date to release so that GPG can work
-/bin/date -s "2025-02-28 12:00"
-
 # Wait for the external MicroSD to mount before checking for a dev build.
 MICROSD_MOUNTPOINT="/mnt/microsd"
 MICROSD_DEV_DIR="$MICROSD_MOUNTPOINT/seedsigner-dev"
@@ -33,5 +30,5 @@ fi
 #/usr/bin/python3 main.py >> /dev/kmsg 2>&1 &  # version that writes output to dmesg
 /usr/bin/python3 main.py &
 
-# Import the bundle of keys a few seconds after the app starts
-(sleep 5 && /usr/bin/gpg --import /gpg/*.asc) &
+# Set the date to release so that GPG can work
+/bin/date -s "2025-02-28 12:00"
