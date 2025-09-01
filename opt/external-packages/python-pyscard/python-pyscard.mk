3c1eac870f9846ba5038f4880be24b775478117c
3rd Iteration <stevekr@gmail.com>
2025-09-01 14:19:48 -0400

    Ensure host swig is on PATH for pyscard

--- opt/external-packages/python-pyscard/python-pyscard.mk
+++ opt/external-packages/python-pyscard/python-pyscard.mk
@@ -8,7 +8,10 @@ PYTHON_PYSCARD_VERSION = 2.3.0
 PYTHON_PYSCARD_SITE = $(call github,LudovicRousseau,pyscard,$(PYTHON_PYSCARD_VERSION))
 PYTHON_PYSCARD_SETUP_TYPE = setuptools
 PYTHON_PYSCARD_LICENSE = LGPL
-PYTHON_PYSCARD_DEPENDENCIES = host-swig
+# Ensure the swig host tool is built before running the Python build
+PYTHON_PYSCARD_DEPENDENCIES += host-swig
+# And expose it on PATH for the metadata generation step
+PYTHON_PYSCARD_ENV = PATH="$(HOST_DIR)/bin:$(HOST_DIR)/sbin:$$PATH"
 
  
  $(eval $(python-package))
