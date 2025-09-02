46039dde5c720049e35435c77c5dcfa73e82857c
3rd Iteration <stevekr@gmail.com>
2025-09-01 20:20:20 -0400

    Use host Buildroot swig for pyscard

--- opt/external-packages/python-pyscard/python-pyscard.mk
+++ opt/external-packages/python-pyscard/python-pyscard.mk
@@ -6,13 +6,19 @@
 
 PYTHON_PYSCARD_VERSION = 2.3.0
 PYTHON_PYSCARD_SITE = $(call github,LudovicRousseau,pyscard,$(PYTHON_PYSCARD_VERSION))
-PYTHON_PYSCARD_SETUP_TYPE = setuptools
-PYTHON_PYSCARD_LICENSE = LGPL
-# Ensure the swig host tool is available for wrapper generation
-PYTHON_PYSCARD_DEPENDENCIES += host-python-swig-pypi
-# Explicitly point setup.py to the host-provided swig binary
-PYTHON_PYSCARD_ENV += SWIG="$(HOST_DIR)/bin/swig"
+PYTHON_PYSCARD_SETUP_TYPE = pep517
+PYTHON_PYSCARD_LICENSE = LGPL-2.1+
 
- 
- $(eval $(python-package))
+# Need SWIG at build time and pcsc-lite headers/libs
+PYTHON_PYSCARD_DEPENDENCIES = host-swig pcsc-lite
+
+# Expose the host-provided swig binary and pkg-config path
+PYTHON_PYSCARD_ENV = \
+	SWIG=$(HOST_DIR)/bin/swig \
+	PKG_CONFIG_PATH=$(STAGING_DIR)/usr/lib/pkgconfig
+
+# Disable build isolation so the backend can see host tools
+PYTHON_PYSCARD_PEP517_BUILD_OPTS = --no-build-isolation
+
+$(eval $(python-package))
 
