b711c669e73b4f4e1f2e73833bab7374c11a93f7
3rd Iteration <steve@cryptoguide.tips>
2025-11-07 11:13:53 -0300

    Update pyscard to 2.3.1 (#49)

--- opt/external-packages/python-pyscard/python-pyscard.mk
+++ opt/external-packages/python-pyscard/python-pyscard.mk
@@ -4,7 +4,7 @@
  #
  ################################################################################
 
-PYTHON_PYSCARD_VERSION = 2.3.0
+PYTHON_PYSCARD_VERSION = 2.3.1
 PYTHON_PYSCARD_SITE = $(call github,LudovicRousseau,pyscard,$(PYTHON_PYSCARD_VERSION))
 PYTHON_PYSCARD_SETUP_TYPE = setuptools
 PYTHON_PYSCARD_LICENSE = LGPL
