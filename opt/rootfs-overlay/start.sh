9e2ccdddd96d61218361d7bb1542ec2d60e4d6e6
3rd Iteration <stevekr@gmail.com>
2026-01-06 17:02:02 +1000

    Use build commit time as default clock

--- opt/rootfs-overlay/start.sh
+++ opt/rootfs-overlay/start.sh
@@ -6,9 +6,17 @@ cd /opt/src/
 /usr/bin/python3 main.py &
 
 # Set the date to release so that GPG can work
+TIME_DEFAULT_FILE="/opt/src/.build_commit_time"
 TIME_FALLBACK="2025-02-28 12:00"
 TIME_FILE="/mnt/microsd/time.txt"
 TIME_VALUE="$TIME_FALLBACK"
+
+if [ -f "$TIME_DEFAULT_FILE" ]; then
+    TIME_FROM_DEFAULT=$(tr -d '\r\n' < "$TIME_DEFAULT_FILE")
+    if [ -n "$TIME_FROM_DEFAULT" ]; then
+        TIME_VALUE="$TIME_FROM_DEFAULT"
+    fi
+fi
 
 if [ -f "$TIME_FILE" ]; then
     TIME_FROM_FILE=$(tr -d '\r\n' < "$TIME_FILE")
